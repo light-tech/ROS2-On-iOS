e39f297de66efb1c7b1be370b29439dfb4f62f7b
Lightech <lightech@outlook.com>
2022-08-09 09:33:20 -0400

    Force THIRDPARTY in fastrtps to use its copy of asio, libxml2

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ Guides:
  * https://docs.ros.org/en/humble/Installation/Alternatives/macOS-Development-Setup.html
  * https://docs.ros.org/en/humble/How-To-Guides/Cross-compilation.html
 
+Steps:
+
+ 1. Create a new Python virtual environment
+    ```shell
+    export MY_PYTHON_ENV=usr_local
+    python3 -m venv $MY_PYTHON_ENV
+    ```
+    Here I use `usr_local` as it is intended to act like `/usr/local/`.
+
+ 2. Activate the environment
+    ```shell
+    source $MY_PYTHON_ENV/bin/activate
+    ```
+
+ 3. Install the packages
--- build_ros2.sh
+++ build_ros2.sh
@@ -1,4 +1,4 @@
-REPO_ROOT=`pwd`
+RREPO_ROOT=`pwd`
 
 python3 -m pip install --user -U \
  argcomplete catkin_pkg colcon-common-extensions coverage \
@@ -9,11 +9,17 @@ python3 -m pip install --user -U \
  nose pep8 psutil pydocstyle pydot pyparsing==2.4.7 \
  pytest-mock rosdep rosdistro setuptools==59.6.0 vcstool #pygraphviz
 
+# Temporarily use the GitHub Action user script installation location
 export PATH=$PATH:/Users/runner/Library/Python/3.9/bin
 
+# Download local ASIO
+# git clone https://github.com/chriskohlhoff/asio
+
+cd $REPO_ROOT
 mkdir -p src
 wget https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos
 vcs import src < ros2.repos
 
 cd $REPO_ROOT
-colcon build --symlink-install --packages-skip-by-dep python_qt_binding
+colcon build --symlink-install --packages-skip-by-dep python_qt_binding \
+    --cmake-args -DTHIRDPARTY=FORCE # -DAsio_INCLUDE_DIR=$REPO_ROOT/asio/asio/include/
