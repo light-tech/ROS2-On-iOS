a17d644b5967ebbb2202765dca12a87f301fccc1
Lightech <lightech@outlook.com>
2022-08-11 10:21:39 -0400

    Add minimum set of repos

--- build_ros2.sh
+++ build_ros2.sh
@@ -6,7 +6,7 @@ REPO_ROOT=`pwd`
 cd $REPO_ROOT
 mkdir src
 # wget https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos
-vcs import src < $REPO_ROOT/ros2.repos
+vcs import src < $REPO_ROOT/ros2_min.repos
 
 cd $REPO_ROOT
-colcon build --symlink-install --packages-skip-by-dep python_qt_binding --cmake-args -DTHIRDPARTY=FORCE -DFORCE_BUILD_VENDOR_PKG=ON # --executor sequential --event-handlers console_direct+ -DAsio_INCLUDE_DIR=$REPO_ROOT/asio/asio/include/
+colcon build --symlink-install --packages-skip-by-dep python_qt_binding --cmake-args -DTHIRDPARTY=FORCE -DFORCE_BUILD_VENDOR_PKG=ON -DBUILD_SHARED_LIBS=NO -DBUILD_TESTING=NO # --executor sequential --event-handlers console_direct+ -DAsio_INCLUDE_DIR=$REPO_ROOT/asio/asio/include/
