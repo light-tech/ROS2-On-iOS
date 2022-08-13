0b5a91824cd33edb550609d2de95646e373df403
Lightech <lightech@outlook.com>
2022-08-12 22:18:12 -0400

    Initiate ROS2 example iOS app

---
+++ ROS2Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ROS2Example
+//
+//  Created by Lightech on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ROS2Example/ROS2ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROS2ExampleApp.swift
+//  ROS2Example
+//
+//  Created by Lightech on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ROS2ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
