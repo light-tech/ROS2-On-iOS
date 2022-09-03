2971dbc8f8cc17faf5b6119d1ac9cb4033a12a54
Lightech <lightech@outlook.com>
2022-09-02 22:43:50 -0400

    Build Eigen3 after SuiteSparse

--- build_deps.sh
+++ build_deps.sh
@@ -53,6 +53,7 @@ function getAndExtractSource() {
     mkdir -p $SRC_PATH
     cd $SRC_PATH
     local src_files=`ls $DOWNLOAD_PATH`
+    ls -all $DOWNLOAD_PATH
     for f in $src_files; do
         tar xzf $DOWNLOAD_PATH/$f
     done
@@ -84,31 +85,31 @@ function buildCMake() {
 }
 
 function configureThenMakeArm() {
-    export CFLAGS="-isysroot $SYSROOT -arch $ARCH"
-    export CPPFLAGS="-isysroot $SYSROOT -arch $ARCH"
+    export CFLAGS="-isysroot $SYSROOT -arch $ARCH $CFLAGS"
+    export CPPFLAGS="-isysroot $SYSROOT -arch $ARCH $CPPFLAGS"
 
     if [ -z "$MAKE_HOST_TRIPLE_ZLIB" ]
     then
-        ./configure --prefix=$DEPS_SYSROOT --host=$MAKE_HOST_TRIPLE_ZLIB "$@" >/dev/null 2>&1
+        ./configure --prefix=$DEPS_SYSROOT "$@" # >/dev/null 2>&1
     else
-        ./configure --prefix=$DEPS_SYSROOT "$@" >/dev/null 2>&1
+        ./configure --prefix=$DEPS_SYSROOT --host=$MAKE_HOST_TRIPLE_ZLIB "$@" # >/dev/null 2>&1
     fi
 
-    make && make install >/dev/null 2>&1
+    make && make install # >/dev/null 2>&1
 
     export -n CFLAGS
     export -n CPPFLAGS
 }
 
 function configureThenMake() {
-    export CFLAGS="-isysroot $SYSROOT -arch $ARCH"
-    export CPPFLAGS="-isysroot $SYSROOT -arch $ARCH"
+    export CFLAGS="-isysroot $SYSROOT -arch $ARCH $CFLAGS"
+    export CPPFLAGS="-isysroot $SYSROOT -arch $ARCH $CPPFLAGS"
 
     if [ -z "$MAKE_HOST_TRIPLE" ]
     then
-        ./configure --prefix=$DEPS_SYSROOT --host=$MAKE_HOST_TRIPLE "$@" >/dev/null 2>&1
+        ./configure --prefix=$DEPS_SYSROOT "$@" # >/dev/null 2>&1
     else
-        ./configure --prefix=$DEPS_SYSROOT "$@" >/dev/null 2>&1
+        ./configure --prefix=$DEPS_SYSROOT --host=$MAKE_HOST_TRIPLE "$@" # >/dev/null 2>&1
     fi
 
     make && make install >/dev/null 2>&1
@@ -163,64 +164,74 @@ function buildHostTools() {
 
 function main() {
     # Build zlib
+    echo "Build zlib"
     cd $SRC_PATH/zlib-1.2.12
     configureThenMakeArm
 
     # Build TinyXML2
+    echo "Build TinyXML2"
     cd $SRC_PATH/tinyxml2-9.0.0
     buildCMake
 
     # Build libpng (needs: zlib)
+    echo "Build libpng"
     cd $SRC_PATH/libpng-1.6.37
     configureThenMake
 
     # Build pixman (needs: libpng)
+    echo "Build pixman"
     cd $SRC_PATH/pixman-0.40.0
     configureThenMakeArm
 
     # Build FreeType
+    echo "Build freetype"
     cd $SRC_PATH/freetype-2.12.1
     buildCMake -DFT_DISABLE_HARFBUZZ=ON -DFT_DISABLE_BZIP2=ON
 
     # Build cairo (needs: FreeType, pixman)
+    echo "Build cairo"
     cd $SRC_PATH/cairo-1.16.0
     export PKG_CONFIG=$DEPS_SYSROOT/bin/pkg-config
     export PKG_CONFIG_PATH=$DEPS_SYSROOT/lib/pkgconfig
     configureThenMakeArm --disable-xlib --enable-svg=no --with-sysroot=$DEPS_SYSROOT CFLAGS="-I$DEPS_SYSROOT/include/freetype2" CPPFLAGS="-I$DEPS_SYSROOT/include/freetype2"
 
     # Build Bullet3
+    echo "Build Bullet3"
     cd $SRC_PATH/bullet3-3.24
     buildCMake -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_UNIT_TESTS=OFF
 
-    # Build Eigen3
-    cd $SRC_PATH/eigen-3.4.0
-    buildCMake
-
     # Build gflags
+    echo "Build Bullet3"
     cd $SRC_PATH/gflags-2.2.2
     buildCMake
 
     # Build glog (needs: gflags)
+    echo "Build Bullet3"
     cd $SRC_PATH/glog-0.6.0
     buildCMake -DWITH_GTEST=OFF -DBUILD_TESTING=OFF
 
     # Build GoogleTest
+    echo "Build GoogleTest"
     cd $SRC_PATH/googletest-release-1.12.1
     buildCMake
 
     # Build ABSL
+    echo "Build ABSL"
     cd $SRC_PATH/abseil-cpp-20220623.0
     buildCMake -DCMAKE_CXX_STANDARD=14
 
     # Build GMP
+    echo "Build GMP"
     cd $SRC_PATH/gmp-6.2.1
     configureThenMake
 
     # Build MPFR (needs: GMP)
+    echo "Build MPFR"
     cd $SRC_PATH/mpfr-4.1.0
     configureThenMake --with-gmp=$DEPS_SYSROOT
 
     # Build ProtoBuf
+    echo "Build ProtoBuf"
     cd $SRC_PATH/protobuf-3.21.5
     buildCMake -Dprotobuf_BUILD_TESTS=OFF
 
@@ -228,35 +239,47 @@ function main() {
     case $PLATFORM in
         "macOS")
             # Build Lua
+            echo "Build Lua"
             cd $SRC_PATH/lua-5.4.4
             make macosx
             make install INSTALL_TOP=$DEPS_SYSROOT
 
             # Build Boost
+            echo "Build Boost"
             cd $SRC_PATH/boost_1_80_0
             ./bootstrap.sh --prefix=$DEPS_SYSROOT --without-libraries=python
             ./b2 install
 
             # Build Qt5
+            echo "Build Qt5"
             cd $SRC_PATH/qtbase-everywhere-src-5.15.5
             buildQt5;;
     esac
 
     # Build SuiteSparse
+    echo "Build SuiteSparse"
     cd $SRC_PATH/SuiteSparse-5.12.0
     sed -i.bak 's/#define IDXTYPEWIDTH 64/#define IDXTYPEWIDTH 32/' metis-5.1.0/include/metis.h
     make library CF="-isysroot $SYSROOT -arch $ARCH -I $DEPS_SYSROOT/include" LDFLAGS="-L$DEPS_SYSROOT/lib"
     make install INSTALL=$DEPS_SYSROOT CF="-I $DEPS_SYSROOT/include" LDFLAGS="-L$DEPS_SYSROOT/lib"
 
+    # Build Eigen3 (needs: SuiteSparse even though it is optional)
+    echo "Build Eigen3"
+    cd $SRC_PATH/eigen-3.4.0
+    buildCMake
+
     # Build CERES solver (needs: gflags, glog, SuiteSparse)
+    echo "Build CERES"
     cd $SRC_PATH/ceres-solver-2.0.0
     buildCMake
 
     # Build FLANN
+    echo "Build FLANN"
     cd $SRC_PATH/flann-1.9.1
     buildCMake -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_MATLAB_BINDINGS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF
 
     # Build Point Cloud Library (PCL, needs: Boost, FLANN, Eigen3)
+    echo "Build PCL"
     cd $SRC_PATH/pcl
     buildCMake
 }
