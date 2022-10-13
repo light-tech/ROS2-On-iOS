8fc36be8539c7bfced4983b53375a6f409f2d5a2
Lightech <lightech@outlook.com>
2022-10-13 10:52:35 -0400

    Fix hardcoded ROS2 install dir

--- fix_hardcoded_paths.sh
+++ fix_hardcoded_paths.sh
@@ -1,34 +1,42 @@
-localPythonEnvPath=$1 # e.g. $HOME/usr/ros2PythonEnv
-localLibPythonPath=$2 # e.g. /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/config-3.10-darwin/libpython3.10.dylib
+installDir=$1           # path to the ROS2 installation location e.g. $HOME/usr/ros2
+localPythonEnvPath=$2   # path to virtual Python environment on local machine e.g. $HOME/usr/ros2PythonEnv
+localLibPythonPath=$3   # path to libpython dylib e.g. /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/config-3.10-darwin/libpython3.10.dylib
 
 fixHardCodedPythonEnvPath() {
     hardcodedPythonEnvPath=/Users/runner/work/ROS2-On-iOS/ROS2-On-iOS/ros2PythonEnv
+    hardcodedFiles=($(grep -r -l --include="*.cmake" "$hardcodedPythonEnvPath" .))
 
-    echo "Replacing $hardcodedPythonEnvPath with $localPythonEnvPath"
-
-    # List down files which hard-coded the python environment paths on GitHub Action
-    hardcodedFiles=$(grep -r -l "$hardcodedPythonEnvPath" .)
-
-    # Replace the hardcoded path in those files
     for f in $hardcodedFiles; do
-        echo "In file $f"
-        sed -i.bak "s,$hardcodedPythonEnvPath,$localPythonEnvPath,g" $f
+        echo "Replacing $hardcodedPythonEnvPath -> $localPythonEnvPath in file $f"
+        sed -i '' "s,$hardcodedPythonEnvPath,$localPythonEnvPath,g" $f
     done
 }
 
 fixHardCodedLibPythonPath() {
     local hardcodedLibPythonPath=/usr/local/opt/python@3.10/Frameworks/Python.framework/Versions/3.10/lib/python3.10/config-3.10-darwin/libpython3.10.dylib
+    local hardcodedFiles=($(grep -r -l --include="*.cmake" "$hardcodedLibPythonPath" .))
 
-    local hardcodedFiles=$(grep -r -l "$hardcodedLibPythonPath" .)
-
-    echo $hardcodedFiles
-
-    # Replace the hardcoded path in those files
     for f in $hardcodedFiles; do
-        echo "In file $f"
-        sed -i.bak "s,$hardcodedLibPythonPath,$localLibPythonPath,g" $f
+        echo "Replacing $hardcodedLibPythonPath -> $localLibPythonPath in file $f"
+        sed -i '' "s,$hardcodedLibPythonPath,$localLibPythonPath,g" $f
     done
 }
+
+fixHardCodedInstallDir() {
+    local hardcodedInstallDir=/Users/runner/work/ROS2-On-iOS/ROS2-On-iOS/ros2_macOS
+    local hardcodedFiles=($(grep -r -l --include="*.cmake" --include="*.pc" "$hardcodedInstallDir" .))
+
+    for f in $hardcodedFiles; do
+        echo "Replace hardcoded path $hardcodedInstallDir -> $installDir in file $f"
+        sed -i '' "s,$hardcodedInstallDir,$installDir,g" $f
+    done
+}
+
+fixXcodeFrameworkPath() {
+    echo "You also need to change the OpenGL.framework path in $installDir/share/rviz_default_plugins/cmake/rviz_default_pluginsExport.cmake to match your Xcode's installation."
+}
+
+cd $installDir
 
 echo ""
 echo "Fix hardcoded Python environment path ..."
@@ -40,8 +48,9 @@ echo "Fix hardcoded path to libpython*.dylib ..."
 echo ""
 fixHardCodedLibPythonPath
 
-# Delete the backup files generated
 echo ""
-echo "Delete backup files generated (make sure they are safe to delete, skip if not sure) ..."
+echo "Fix hardcoded ROS2 and dependencies installation location ..."
 echo ""
-find . -name \*.bak -ok rm {} \;
+fixHardCodedInstallDir
+
+fixXcodeFrameworkPath
